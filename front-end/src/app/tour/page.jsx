61242788c8007420ab7ddb19b9d13becec9ec2c5
Тэмүүлэн Эрдэнэбулаг <24LP0976@G401-CC0212.local>
2024-12-18 20:44:43 +0800

    ...

--- front-end/src/app/tour/page.jsx
+++
@@ -1,5 +0,0 @@
-import ToursPage from "@/components/tours-page/Tours";
-
-export default function Products() {
-  return <ToursPage />;
-}
---
+++ front-end/src/app/view-all/[id]/page.jsx
@@ -0,0 +1,7 @@
+import TourPage from "@/components/tours-page/Tours";
+
+const Tour = () => {
+  return <TourPage />;
+};
+
+export default Tour;
