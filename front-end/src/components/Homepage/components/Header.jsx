bc57d6689dfd2811f70b32dc99376428be499be3
Your Name <you@example.com>
2024-12-19 10:06:18 +0800

    tengis

--- front-end/src/components/Homepage/components/Header.jsx
+++ front-end/src/components/Homepage/components/Header.jsx
@@ -66,61 +66,65 @@ export const HeaderPart = ({ home, customTour, event, aboutUs }) => {
             >
               {isMenuOpen ? "✖" : "☰"}
             </button>
-            {isMenuOpen && (
-              <nav className="absolute top-0 left-0 w-full bg-gray-800 text-white justify-end items-center flex flex-col gap-8 p-4">
-                <button
-                  className="text-white text-2xl focus:outline-none absolute top-4 right-4"
-                  aria-label="Close Menu"
-                  onClick={() => setIsMenuOpen(false)}
+
+            <nav
+              className={`absolute top-0 left-0 w-full bg-white text-black justify-end items-center flex flex-col gap-8 p-4 transition-transform duration-500 ${
+                isMenuOpen ? "translate-y-0" : "-translate-y-full"
+              }`}
+            >
+              <button
+                className="text-black text-2xl focus:outline-none absolute top-4 right-4"
+                aria-label="Close Menu"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                ✖
+              </button>
+
+              <Link href="/" passHref>
+                <h1
+                  className={`text-lg font-medium transition duration-300 ${
+                    home
+                      ? "text-orange-500"
+                      : "text-black hover:text-orange-500"
+                  }`}
                 >
-                  ✖
-                </button>
-                <Link href="/" passHref>
-                  <h1
-                    className={`text-lg font-medium transition duration-300 ${
-                      home
-                        ? "text-orange-500"
-                        : "text-white hover:text-orange-500"
-                    }`}
-                  >
-                    Mongolian Tour
-                  </h1>
-                </Link>
-                <Link href="/special-order" passHref>
-                  <h1
-                    className={`text-lg font-medium transition duration-300 ${
-                      customTour
-                        ? "text-orange-500"
-                        : "text-white hover:text-orange-500"
-                    }`}
-                  >
-                    Create Your Tour
-                  </h1>
-                </Link>
-                <Link href="/event-page" passHref>
-                  <h1
-                    className={`text-lg font-medium transition duration-300 ${
-                      event
-                        ? "text-orange-500"
-                        : "text-white hover:text-orange-500"
-                    }`}
-                  >
-                    Mongolian Event
-                  </h1>
-                </Link>
-                <Link href="/about-us" passHref>
-                  <h1
-                    className={`text-lg font-medium transition duration-300 ${
-                      aboutUs
-                        ? "text-orange-500"
-                        : "text-white hover:text-orange-500"
-                    }`}
-                  >
-                    About Us
-                  </h1>
-                </Link>
-              </nav>
-            )}
+                  Mongolian Tour
+                </h1>
+              </Link>
+              <Link href="/special-order" passHref>
+                <h1
+                  className={`text-lg font-medium transition duration-300 ${
+                    customTour
+                      ? "text-orange-500"
+                      : "text-black hover:text-orange-500"
+                  }`}
+                >
+                  Create Your Tour
+                </h1>
+              </Link>
+              <Link href="/event-page" passHref>
+                <h1
+                  className={`text-lg font-medium transition duration-300 ${
+                    event
+                      ? "text-orange-500"
+                      : "text-black hover:text-orange-500"
+                  }`}
+                >
+                  Mongolian Event
+                </h1>
+              </Link>
+              <Link href="/about-us" passHref>
+                <h1
+                  className={`text-lg font-medium transition duration-300 ${
+                    aboutUs
+                      ? "text-orange-500"
+                      : "text-black hover:text-orange-500"
+                  }`}
+                >
+                  About Us
+                </h1>
+              </Link>
+            </nav>
           </div>
         </div>
       </header>
