0827b053335598ac1ef1d5a72b2d777016625b0b
Түвшин Эрдэнэбат <24LP0845@G401-CC0213.local>
2025-01-03 12:35:55 +0800

    changes

--- front-end/src/app/layout.js
+++ front-end/src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,6 +22,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Tour Mongolia</title>
+        <meta
+          name="description"
+          content="Discover Mongolia's breathtaking Gobi Desert with our guided tours. Experience nomadic culture, stunning landscapes, and unforgettable adventures in the heart of Asia. Perfect for travelers seeking unique journeys. Tour Mongolia, Travel Mongolia, Монголд аялах, Mongolia tour, mongoia travel, adventure mongolia, Mongolia Adventure, Gobi tour, gobi, Gobi Mongolia, golden gobi, Cashmere, Eternal, Nomadic, Mongola, Mongol, Hunnu, Chingiss Khan, Mongol Empire, Mongolian Tours"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
